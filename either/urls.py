47a3ceec72bb1296ba1fd00d06734d2bfe31989d
남선웅 <nsw1107@naver.com>
2019-10-16 09:35:42 +0900

    모델완성

--- either/urls.py
+++ either/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from questions import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('questions/',include('questions.urls')),
+    path('',views.index),
 ]
