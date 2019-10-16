b0917705b5415393221eaca67ef29c5c0bae5aa0
남선웅 <nsw1107@naver.com>
2019-10-16 11:14:34 +0900

    Choice 모델 구성

--- questions/models.py
+++ questions/models.py
@@ -6,3 +6,8 @@ class Question(models.Model):
     title = models.CharField(max_length=100)
     choice_1 = models.CharField(max_length=100)
     choice_2 = models.CharField(max_length=100)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    pick = models.IntegerField()
+    comment = models.CharField(max_length=100)
